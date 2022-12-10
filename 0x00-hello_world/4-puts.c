5082b2dfd8690e1a0a795a04df6603fcd7fe5c97
ugwuoko victory <michealvictory680@gmail.com>
2022-12-10 04:19:30 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
