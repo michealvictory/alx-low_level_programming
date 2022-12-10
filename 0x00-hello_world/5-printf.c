480a9adc68a52ea120443b3694487005f2df87ce
ugwuoko victory <michealvictory680@gmail.com>
2022-12-10 04:23:59 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
- */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	printf
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
