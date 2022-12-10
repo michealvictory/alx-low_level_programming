2caab4e1e2edc078343908a84c8309b04a31e187
ugwuoko victory <michealvictory680@gmail.com>
2022-12-10 02:18:27 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ int main(void)
 	int num;
 
 	srand(time(0));
-	n - rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	printf("last digit of %d is ", n);
-	num - n % 10;
+	num = n % 10;
 	if (num > 5)
 	{
 		printf("%d and is greater than 5\n", num);
