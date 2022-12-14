55323e9677edd657ad29f07abd1a43f985b3ff77
ugwuoko victory <michealvictory680@gmail.com>
2022-12-13 18:17:18 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,9 @@
 /**
  * Description: 2-print_alphabet_x10 - prints alpha 10 times
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int ten;
 	char la;
