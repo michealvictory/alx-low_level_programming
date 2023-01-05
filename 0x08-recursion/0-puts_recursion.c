7ed9b8237ce6cb651ba138d618dd9867720cf608
ugwuoko victory <michealvictory680@gmail.com>
2023-01-04 18:34:25 -0800

    ssg

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	}
-	else
-	{
-	_putchar('\n');
+		_putchar('\n');
+		return;
 	}
 
-}
+	_putchar (*s);
+	_puts_recursion(s + 1);
+
+} /*jonesink*/
