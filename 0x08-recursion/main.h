2d4fb3310348cf897df122575505737dc8180e9c
ugwuoko victory <michealvictory680@gmail.com>
2023-01-04 18:41:05 -0800

    dss

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,6 @@
-ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
-#include <stdio.h>
-#include <stdlib.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -13,4 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 #endif
