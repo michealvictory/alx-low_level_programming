2b791f5581025e5ecbe03f3cb6d8fdfe5171f11c
ugwuoko victory <michealvictory680@gmail.com>
2023-01-09 06:13:29 -0800

    lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,19 +1,13 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
- * main - a program that prints __putchar
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: returns 0 always
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
